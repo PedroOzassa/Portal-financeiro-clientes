00b668b1c0b69e1178c32e377221cf290dd14549
PedroOzassa <162368884+PedroOzassa@users.noreply.github.com>
2025-11-04 09:06:19 -0300

    Template de menu adicionado
    menu.html adicionado aos templates e nas rotas

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,18 +1,29 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from flaskr import app
 from flaskr.forms import LoginForm
 
 @app.route('/')
-@app.route('/login', methods=['GET', 'POST']) #TODO: See the necessity of the GET method here, remove if not needed
+@app.route('/index')
+def index():
+    return redirect('/login')
 
+@app.route('/login', methods=['GET', 'POST']) #TODO: See the necessity of the GET method here, remove if not needed
 def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        remember_me = form.remember_me.data
+        #username = form.username.data
+        #password = form.password.data
+        #remember_me = form.remember_me.data
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/dashboard')
         #TODO: Implement login logic here
         # return redirect(url_for('index'))
 
     return render_template('login.html', form=form)
+
+@app.route('/menu')
+def dashboard():
+    return render_template('menu.html')
